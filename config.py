2814cb1f0a3995bba49822fd47f95aaf51059b16
Armin Schulz <mail@arminschulz.com>
2025-04-17 15:52:41 +0200

    add requirements.yml

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-DRAW_LENGTH: int = 50      # --> length of drawn line; max 3 * 255
-THICKNESS: int = 50        # --> maximal thickness of drawn line and line start
-JUMP_LIMIT: int = 200   # --> tolerated distance of finger position between two frames (smaller value prevents jumps between right and left)
-COOLDOWN_AUTO: int = 250        # --> number of frames that have to pass since the last made picture until an automatic picture is made
-COOLDOWN_MANUAL: int = 25       # --> minimal number of frames between two manual triggered pictures
+DRAW_LENGTH: int = 50       # --> length of drawn line; max 3 * 255
+THICKNESS: int = 50         # --> maximal thickness of drawn line and line start
+JUMP_LIMIT: int = 200       # --> tolerated distance of finger position between two frames (smaller value prevents jumps between right and left)
+COOLDOWN_AUTO: int = 250    # --> number of frames that have to pass since the last made picture until an automatic picture is made
+COOLDOWN_MANUAL: int = 25   # --> minimal number of frames between two manual triggered pictures
 
 DEFAULT_MODE: str = 'painting'
 ###
